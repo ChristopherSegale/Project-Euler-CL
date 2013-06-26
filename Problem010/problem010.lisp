1cabe8c03dd4c7591fde9472e35deb44474c222e
ChristopherSegale <ccsp.segale@gmail.com>
2013-06-26 09:14:20 -0400

    Solving problem 10

---
+++ Problem010/problem010.lisp
@@ -0,0 +1,23 @@
+;;;; Author: Christopher Segale
+;;;; Date: 6/26/2013
+
+(defun get-primes (limit)
+  (setf candidates (loop for i from 2 to limit if (or (= i 2) (oddp i)) collect i))
+  (setf n 3)
+
+  (loop for n in candidates
+    while (< (* n n) limit) do
+      (delete-if #'(lambda (f) (and (not (= f n)) (= 0 (mod f n)))) candidates))
+
+  (return-from get-primes candidates))
+
+(defun problem010 ()
+  (setf primes (get-primes 2000000))
+
+  (loop for i in primes
+    sum i))
+
+(defun main ()
+  (print (problem010)))
+
+(main)
