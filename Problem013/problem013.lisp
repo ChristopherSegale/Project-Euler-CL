3fc1a2a9d65057fc26dfedb1a4de9f5b64d2fcf8
ChristopherSegale <ccsp.segale@gmail.com>
2013-06-27 19:04:43 -0400

    Solving problem 13

---
+++ Problem013/problem013.lisp
@@ -0,0 +1,23 @@
+;;;; Author: Christopher Segale
+;;;; Date: 6/27/2013
+
+(defun get-list (file-name)
+  (let ((in (open file-name :if-does-not-exist nil)))
+    (if in
+      (loop for line = (read-line in nil)
+         while line collect (parse-integer line))
+      (close in))))
+
+(defun get-sum()
+  (setf numbers (get-list "numbers.txt"))
+
+  (loop for n in numbers
+    sum n))
+
+(defun problem013 ()
+  (subseq (princ-to-string (get-sum)) 0 10))
+
+(defun main ()
+  (format t "~a" (problem013)))
+
+(main)
