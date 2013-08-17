c101b304fb8758ce24b21da4db1a332fc3ff0f5e
ChristopherSegale <ccsp.segale@gmail.com>
2013-08-17 15:55:39 -0400

    Refactored code

--- Problem010/problem010.lisp
+++ Problem010/problem010.lisp
@@ -2,19 +2,19 @@
 ;;;; Date: 6/26/2013
 
 (defun get-primes (limit)
-  (setf candidates (loop for i from 2 to limit if (or (= i 2) (oddp i)) collect i))
+  (let ((candidates (loop for i from 2 to limit if (or (= i 2) (oddp i)) collect i)))
 
-  (loop for n in candidates
-    while (< (* n n) limit) do
-      (delete-if #'(lambda (f) (and (not (= f n)) (= 0 (mod f n)))) candidates))
+    (loop for n in candidates
+      while (< (* n n) limit) do
+        (delete-if #'(lambda (f) (and (not (= f n)) (= 0 (mod f n)))) candidates))
 
-  (return-from get-primes candidates))
+    (return-from get-primes candidates)))
 
 (defun problem010 ()
-  (setf primes (get-primes 2000000))
+  (let ((primes (get-primes 2000000)))
 
-  (loop for i in primes
-    sum i))
+    (loop for i in primes
+      sum i)))
 
 (defun main ()
   (print (problem010)))
