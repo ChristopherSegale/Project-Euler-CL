859e8d0be7013643d339d79e3095a41bb5b19f1c
ChristopherSegale <ccsp.segale@gmail.com>
2013-08-17 15:44:31 -0400

    Refactored code

--- Problem002/problem002.lisp
+++ Problem002/problem002.lisp
@@ -2,17 +2,17 @@
 ;;;; Date: 6/21/2013
 
 (defun get-fibs (limit)
-  (setf fibs '(2 1))
-  (loop do
-    (push (+ (nth 0 fibs) (nth 1 fibs)) fibs)
-   while (< (+ (nth 0 fibs) (nth 1 fibs)) limit))
-  (return-from get-fibs fibs))
+  (let ((fibs '(2 1)))
+    (loop do
+      (push (+ (nth 0 fibs) (nth 1 fibs)) fibs)
+     while (< (+ (nth 0 fibs) (nth 1 fibs)) limit))
+    (return-from get-fibs fibs)))
 
 (defun problem002 ()
-  (setf fibs (get-fibs 4000000))
-  (loop for i in fibs do
-     if (evenp i)
-   sum i))
+  (let ((fibs (get-fibs 4000000)))
+    (loop for i in fibs
+       if (evenp i)
+     sum i)))
 
 (defun main ()
   (format t "~a" (problem002)))
